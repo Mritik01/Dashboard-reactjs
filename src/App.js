5f84ad53209d8948c83c816b7accc4548e41629d
Ritik <egritik.2014@gmail.com>
2023-02-07 17:49:01 +0530

    argon dashboard 1st commit working on dashboard

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import BackgroundColor from './Components/BackgroundColor';
+import SideDashboard from './Components/SideDashboard';
+import TopHeadGrid from './Components/TopHeadGrid';
+import NavBar from './Components/NavBar';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <BackgroundColor />
+      {/* <SideDashboard /> */}
+      <TopHeadGrid />
+      <Footer/>
+    </>
   );
 }
 
---
+++ src/Components/BackgroundColor.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./backgroundColor.css";
+
+function BackgroundColor() {
+    return (
+        <>
+            <div className="bg-primary position-absolute w-100 background-half-color">
+            </div>
+        </>
+    )
+}
+
+export default BackgroundColor;
